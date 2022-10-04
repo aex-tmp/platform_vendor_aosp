0a39fd178b0d258076cee41f2d1ef3114e6b2f6b
Nolen Johnson <johnsonnolen@gmail.com>
2022-10-03 21:16:56 -0400

    common: Exclude ntfs-3g from artifact path requirement
    Change-Id: I6253ac0b326f86ba7fd78f6babb9ee2242ba9133

--- config/common.mk
+++ config/common.mk
@@ -150,6 +150,13 @@ PRODUCT_PACKAGES += \
     rsync \
     zip
 
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/fsck.ntfs \
+    system/bin/mkfs.ntfs \
+    system/bin/mount.ntfs \
+    system/%/libfuse-lite.so \
+    system/%/libntfs-3g.so
+
 # Exchange support
 PRODUCT_PACKAGES += \
     Exchange2
